cc70dc2f89fc27a068787919f656bc41ec32e01a
Emma Roche <emmaroche@Emmas-MBP.wit.ie>
2022-03-08 10:20:30 +0000

    Bug Fix - make action icons on movie card configurable.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -3,6 +3,8 @@ import { getMovies } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+
 
 const HomePage = (props) => {
 
@@ -22,12 +24,15 @@ const HomePage = (props) => {
   localStorage.setItem('favorites', JSON.stringify(favorites))
   const addToFavorites = (movieId) => true 
 
-  return (
-    <PageTemplate
-      title='Discover Movies'
-      movies={movies}
-      selectFavorite={addToFavorites}
-    />
+   return (
+      <PageTemplate
+        title="Discover Movies"
+        movies={movies}
+        action={(movie) => {
+          return <AddToFavoritesIcon movie={movie} />
+        }}
+      />
   );
 };
+
 export default HomePage;
