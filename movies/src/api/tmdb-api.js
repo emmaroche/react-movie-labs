c272c88216faaf13b50554b6a5cc70dd86bddfce
Emma Roche <emmaroche@Emmas-MBP.wit.ie>
2022-03-08 10:49:57 +0000

    Change upcoming movies page to use caching.

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -28,6 +28,7 @@ export const getMovie = (args) => {
     throw error
  });
 };
+
   export const getGenres = async () => {
     return fetch(
       "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
@@ -72,10 +73,16 @@ export const getMovie = (args) => {
       });
   };
 
-  export const getUpcomingMovie = () => {
-    return fetch(
-      `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
-    )
-    .then(res => res.json())
-    .then(json => json.results);
+export const getUpcomingMovie = () => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+  .catch((error) => {
+     throw error
+  });
 };
