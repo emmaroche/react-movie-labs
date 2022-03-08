114802c0545c454aabd5c6a3a9edb857608cf8cb
Emma Roche <emmaroche@Emmas-MBP.wit.ie>
2022-03-08 10:26:49 +0000

    Fix upcoming movies page.

--- movies/src/pages/UpcomingMoviesPage.js
+++ movies/src/pages/UpcomingMoviesPage.js
@@ -1,30 +1,34 @@
 import { getUpcomingMovie } from "../api/tmdb-api";
 import React, { useState, useEffect } from "react";
 import PageTemplate from '../components/templateMovieListPage'
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
 
 const UpcomingMoviesPage = (props) => {
-  const [movies, setMovies] = useState([]);
+  const {  data, error, isLoading, isError }  = useQuery('discover', getUpcomingMovie)
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
+
+  // Redundant, but necessary to avoid app crashing.
   const favorites = movies.filter(m => m.favorite)
   localStorage.setItem('favorites', JSON.stringify(favorites))
-
-  const addToFavorites = (movieId) => {
-    const updatedMovies = movies.map((m) =>
-      m.id === movieId ? { ...m, favorite: true } : m
-    );
-    setMovies(updatedMovies);
-  };
-
-  useEffect(() => {
-    getUpcomingMovie().then(movies => {
-        setMovies(movies);
-    });
-  }, []);
+  const addToFavorites = (movieId) => true 
 
   return (
     <PageTemplate
       title='Upcoming Movies'
       movies={movies}
-      selectFavorite={addToFavorites}
+      action={(movie) => {
+        return <AddToFavoritesIcon movie={movie} />
+      }}
     />
   );
 };
